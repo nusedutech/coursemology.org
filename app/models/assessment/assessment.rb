e55c195a43e281d24ff757027d2b1b116f502cd6
hiepluonghlv <hiepluonghlv@gmail.com>
2015-01-13 15:03:27 +0800

    fixed can manage Assessment in loading lesson_plan & overview

--- app/models/assessment/assessment.rb
+++ app/models/assessment/assessment.rb
@@ -173,7 +173,7 @@ class Assessment < ActiveRecord::Base
 		end
   end
 
-  def as_lesson_plan_entry (course, user_course)
+  def as_lesson_plan_entry (course, user_course, manage_assessment)
     entry = LessonPlanEntry.create_virtual
     entry.title = self.title
     entry.description = self.description
@@ -183,12 +183,12 @@ class Assessment < ActiveRecord::Base
     entry.url = get_path
     entry.assessment = self
     entry.is_published = self.published
-    entry.submission = user_course ? get_submission(course, user_course) : nil
+    entry.submission = user_course ? get_submission(course, user_course, manage_assessment) : nil
     entry
   end
 
   #2014-12-18 refactoring from index method of assessment controller (line 50 - 87)
-  def get_submission(course, user_course)
+  def get_submission(course, user_course, manage_assessment)
     result = Hash.new
     sub = self.submissions.where(std_course_id: user_course.id).order('updated_at DESC').first
     dependent_ast_sub = self.dependent_on.nil? ? nil : self.dependent_on.submissions.where(std_course_id: user_course.id).order('updated_at DESC').first
@@ -198,7 +198,7 @@ class Assessment < ActiveRecord::Base
     elsif (self.opened? and (self.as_assessment.class == Assessment::Training or
         self.dependent_id.nil? or self.dependent_id == 0 or
         (!dependent_ast_sub.nil? and !dependent_ast_sub.attempting?))) or
-        can?(:manage, self)
+        manage_assessment
       result[:action] = "Attempt"
       result[:url] = new_course_assessment_submission_path(course, self, from_lesson_plan: true)
     else

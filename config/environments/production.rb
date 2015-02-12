b2bde51477e3a936f5380d5f597c089264eac52e
hiepluonghlv <hiepluonghlv@gmail.com>
2015-02-12 17:26:57 +0800

    update config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Coursemology::Application.configure do
                         :email => {
                             :email_prefix => "[ERROR]",
                             :sender_address => %{"Coursemology Exception" <exception.notifier@coursemology.com>},
-                            :exception_recipients => "nusedutech@gmail.com"
+                            :exception_recipients => "eduthsoc@comp.nus.edu.sg"
                         }
 
   #ivle login, NUS openID login

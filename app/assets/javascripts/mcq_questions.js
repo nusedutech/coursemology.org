49ac1ba64a93a93af68d333b65153ab9f63ef5d3
hiepluonghlv <hiepluonghlv@gmail.com>
2015-04-27 19:42:04 +0800

    enable ckeditor

--- app/assets/javascripts/mcq_questions.js
+++ app/assets/javascripts/mcq_questions.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
               '<td><input name="options[' + num_ans + '][correct]" type="checkbox" /></td>',
               '  <td>',
               '    <textarea id="options_'+num_ans+'_text" name="options[' + num_ans + '][text]" placeholder="Answer..." /></textarea>',
-              //'  </td><td>',
+              '  </td><td>',
               '    <textarea id="options_'+num_ans+'_explanation" name="options[' + num_ans + '][explanation]" placeholder="Explanation..." /></textarea>',
               '  </td>',
               '</tr>'].join('');
     $(this).parents('tr').before(format);
-    //$('textarea#options_'+num_ans+'_text').ckeditor();
-    //$('textarea#options_'+num_ans+'_explanation').ckeditor();
+    $('textarea#options_'+num_ans+'_text').ckeditor();
+    $('textarea#options_'+num_ans+'_explanation').ckeditor();
   });
 });

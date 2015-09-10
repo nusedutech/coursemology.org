ca229d7f67b72000bd55e3abb0a52e1b5b978f26
hiepluonghlv <hiepluonghlv@gmail.com>
2015-09-10 10:47:26 +0800

    loaded missions as realtime mission in session group

--- app/models/assessment/mission.rb
+++ app/models/assessment/mission.rb
@@ -16,6 +16,8 @@ class Assessment::Mission < ActiveRecord::Base
   #TODO
   validates_with DateValidator, fields: [:open_at, :close_at]
 
+  scope :with_session_group, -> { joins(:realtime_session_groups).where("assessment_realtime_session_groups.deleted_at is null") }
+  
   has_many :realtime_session_groups, class_name: Assessment::RealtimeSessionGroup, foreign_key: :mission_id
   has_many :sessions, through: :realtime_session_groups
 

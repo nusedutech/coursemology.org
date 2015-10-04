a77606961608437807d153ef03dfb6b283345a3f
hiepluonghlv <hiepluonghlv@gmail.com>
2015-10-04 20:15:38 +0800

    test ivle 1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,24 @@
+require "omniauth"
+require "faraday"
+require "multi_json"
+
 class StaticPagesController < ApplicationController
   def welcome
     #@courses = Course.online_course.limit(10)
+    t = 0
+    token = request.params["token"]
+    unless token.nil?
+      profile_url = "https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey=mHy1mEcwwWvlHYqc9bNdO&AuthToken=#{token}"
+
+      #conn = Faraday::Connection.new(url: profile_url, :ssl => { :ca_path => "/usr/local/ssl/certs"})
+      conn = Faraday.new(url: profile_url, ssl: { verify: false })
+      #conn = Faraday.new(url: profile_url)
+      response = conn.get
+      logger.debug "response body #{response.body}"
+      json = MultiJson.decode(response.body)
+      @profile = json["Results"][0]
+    end
+
   end
 
   def about
